425bb30d028a6b838dffda2465fcb3f3e1e959e3
injamam00155 <injamam00155@gmail.com>
2023-07-27 15:43:32 +0600

    pdf generation working

--- event/views.py
+++ event/views.py
@@ -104,48 +104,70 @@ from reportlab.lib.units import inch  # Import inch to set dimensions in points
 from reportlab.lib.pagesizes import letter  # Import your desired page size
 from reportlab.lib.pagesizes import letter, landscape
 from reportlab.lib.pagesizes import A4
-
 from reportlab.lib.pagesizes import letter
 from reportlab.lib.utils import ImageReader
 from reportlab.pdfgen.canvas import Canvas
 from django.templatetags.static import static
 from reportlab.graphics.shapes import Path
-
-
+from PIL import Image
+import requests
+import cloudinary.api
 
 
 
 # PARTLY WORKING
 
 
-def svg_to_pdf(svg_code,participant, page_size=letter):
+def svg_to_pdf(svg_code,participant,id_type, page_size=letter):
     # Create a PDF buffer
     pdf_buffer = BytesIO()
-    
-    # Convert SVG to PDF using reportlab's svglib
     drawing = svglib.svg2rlg(BytesIO(svg_code.encode('utf-8')))
-    pdf_canvas = Canvas(pdf_buffer, pagesize=(485.53, 687))  # Set the page size
-    reportlab.graphics.renderPDF.draw(drawing, pdf_canvas, 0, 0)
+    pdf_canvas = Canvas(pdf_buffer, pagesize=(485.53,687 ))  # Set the page size
     
+
+    if(id_type=='own'):
+        image_url=participant.participant_image.url
+    elif(id_type=='spouse'):
+        # Render the SVG template with the spouse's data
+        image_url=participant.spouse_image.url
+
+    split_url = image_url.split("v1/")
+
+    if len(split_url) > 1:
+        image_id = split_url[1]
+    else:
+        print("Invalid URL format")
+
+
+    # Download the image from the URL
+    image_info = cloudinary.api.resource(image_id)
+    
+    # print(image_info)
+    image_url = image_info['url']
+    image_width = image_info['width']
+    image_height = image_info['height']
+
+    # Calculate the scaling factor to maintain the aspect ratio
+    scaling_factor = min(220 / image_width, 220 / image_height)
+
+    # Calculate the scaled width and height
+    scaled_width = image_width * scaling_factor
+    scaled_height = image_height * scaling_factor
+    print(scaled_width,scaled_height)
+    # Calculate the position to center the image on the canvas
+    center_x = (485.53 - scaled_width) / 2
+    center_y = (687 - scaled_height) / 2
+
+    pdf_canvas.drawImage(image_url, center_x, center_y, width=scaled_width, height=scaled_height)
+
+    reportlab.graphics.renderPDF.draw(drawing, pdf_canvas, 0, 0)
     
 
     pdf_canvas.drawImage('static/logo1.png', 32, 550, width=100, height=100, mask='auto')
     pdf_canvas.drawImage('static/logo2.png', 352, 550, width=100, height=100, mask='auto')
 
-    # Define the SVG path for the clipping path
-    
-    # Convert the SVG path to clip path points
-    clip_path_points = [
-        (0, 325),  # Top-left corner
-        (290, 325),  # Top-right corner
-        (0, 500),  # Bottom-right corner
-        (290, 500)   # Bottom-left corner
-]
-    # pdf_canvas.saveState()
-    # canvas.clipPath(clip_path_points, stroke=1, fill=0, fillMode='FILL_EVEN_ODD')
-    # pdf_canvas.drawImage(participant.participant_image.url, 150, 300, width=130, height=130)
-    # pdf_canvas.restoreState()
-    # Save the PDF to the buffer
+
+
     pdf_canvas.save()
 
     # Return the PDF buffer
@@ -190,7 +212,7 @@ def get_id_card(request, id_type, id_number):
         svg_code = render_to_string('id_spouse.html', context)
 
     # Convert SVG to PDF with the desired page size (e.g., letter)
-    pdf_data = svg_to_pdf(svg_code,participant, page_size=letter)  # You can pass other page sizes as needed
+    pdf_data = svg_to_pdf(svg_code,participant,id_type, page_size=letter)  # You can pass other page sizes as needed
     # pdf_data = svg_to_pdf_with_canvas(svg_code, page_size=letter)  # You can pass other page sizes as needed
 
     if pdf_data:
