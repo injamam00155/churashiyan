02cce310a603025c2d7a30bd8f4e2cfcd390e049
injamam00155 <injamam00155@gmail.com>
2023-09-02 05:52:59 +0600

    adding countdown and fixing authentication

--- churashiyan/settings.py
+++ churashiyan/settings.py
@@ -64,7 +64,11 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
 ]
 
-
+# PASSWORD_HASHERS = [
+#     'django.contrib.auth.hashers.Argon2PasswordHasher',
+#     'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+#     # ...
+# ]
 
 ROOT_URLCONF = 'churashiyan.urls'
 
